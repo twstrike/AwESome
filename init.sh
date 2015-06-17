1d181e8798c87baf7ad9a66a71b165f25d1036ae
Ola Bini <ola.bini@gmail.com>
2015-06-17 14:08:48 -0500

    Make the current directory calculation actually understand sourcing

--- init.sh
+++ init.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 # This file SHOULD NOT be executed, it should be sourced.
+CURRENT_SOURCE=$_
+if [[ "$CURRENT_SOURCE" != *init.sh ]]; then
+    echo "For this to work correctly, you need to source this file, not execute it:"
+    echo "  source init.sh"
+else
+    CURRENT_DIR="$(cd "$(dirname "$CURRENT_SOURCE")" && pwd -P)"
+    export GOPATH=$CURRENT_DIR/gopkg
+    export PATH=$GOPATH/bin:$PATH
 
-CURRENT_DIR="$(cd $(dirname $0) && pwd)"
+    mkdir -p $GOPATH
+    mkdir -p $GOPATH/src
+    mkdir -p $GOPATH/pkg
+    mkdir -p $GOPATH/bin
+    mkdir -p $GOPATH/src/github.com/twstrike
 
-export GOPATH=$CURRENT_DIR/gopkg
-export PATH=$GOPATH/bin:$PATH
-
-mkdir -p $GOPATH
-mkdir -p $GOPATH/src
-mkdir -p $GOPATH/pkg
-mkdir -p $GOPATH/bin
-mkdir -p $GOPATH/src/github.com/twstrike
-
-if [ ! -e $GOPATH/src/github.com/twstrike/AwESome ]; then
-    ln -s $CURRENT_DIR $GOPATH/src/github.com/twstrike/AwESome
+    if [ ! -e $GOPATH/src/github.com/twstrike/AwESome ]; then
+        ln -s $CURRENT_DIR $GOPATH/src/github.com/twstrike/AwESome
+    fi
 fi
