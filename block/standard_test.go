ff616daf69144865409b35f54fadacb98af36589
Ola Bini <ola.bini@gmail.com>
2015-06-17 14:38:50 -0500

    Add a standard test for CTR 128

--- block/standard_test.go
+++ block/standard_test.go
@@ -108,3 +108,26 @@ var cbcAES128Vector = TestVector{
 		},
 	},
 }
+
+var ctrAES128Vector = TestVector{
+	"2b7e151628aed2a6abf7158809cf4f3c",
+	"f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff",
+	[]Block{
+		Block{
+			"6bc1bee22e409f96e93d7e117393172a",
+			"874d6191b620e3261bef6864990db6ce",
+		},
+		Block{
+			"ae2d8a571e03ac9c9eb76fac45af8e51",
+			"9806f66b7970fdff8617187bb9fffdff",
+		},
+		Block{
+			"30c81c46a35ce411e5fbc1191a0a52ef",
+			"5ae4df3edbd5d35e5b4f09020db03eab",
+		},
+		Block{
+			"f69f2445df4f9b17ad2b417be66c3710",
+			"1e031dda2fbe03d1792170a0f3009cee",
+		},
+	},
+}
