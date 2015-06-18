cf5780f7fb4ff845377920428c458504caeed2aa
Iván Pazmiño <iapazmino@gmail.com>
2015-06-18 13:16:35 -0500

    Add a mixColumns implementation

--- aes/mixcolumns.go
+++ aes/mixcolumns.go
@@ -1,6 +1,36 @@
 package aes
 
+func mul1(b byte) byte {
+	return b
+}
+
+func mul2(b byte) byte {
+	h := byte(int8(b) >> 7)
+	return (b << 1) ^ (0x1B & h)
+}
+
+func mul3(b byte) byte {
+	return b ^ mul2(b)
+}
+
+func mixOneColumn(s state, col int) state {
+	a0 := s[0 + col]
+	a1 := s[4 + col]
+	a2 := s[8 + col]
+	a3 := s[12+ col]
+
+	result := s
+	result[0 + col] = mul2(a0) ^ mul3(a1) ^ mul1(a2) ^ mul1(a3)
+	result[4 + col] = mul1(a0) ^ mul2(a1) ^ mul3(a2) ^ mul1(a3)
+	result[8 + col] = mul1(a0) ^ mul1(a1) ^ mul2(a2) ^ mul3(a3)
+	result[12+ col] = mul3(a0) ^ mul1(a1) ^ mul1(a2) ^ mul2(a3)
+	return result
+}
+
 func mixColumns(s state) state {
-	// TODO: implement
-	return s
+	result := s
+	for i:=0; i<4; i++ {
+		result = mixOneColumn(result, i)
+	}
+	return result
 }
---
+++ aes/mixcolumns_test.go
@@ -0,0 +1,128 @@
+package aes
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+type MixColumnsSuite struct{}
+
+var _ = Suite(&MixColumnsSuite{})
+
+func (s *MixColumnsSuite) Test_mixOneColumnTestVector1(c *C) {
+	before := state{
+		0xDB, 0, 0, 0,
+		0x13, 0, 0, 0,
+		0x53, 0,0,0,
+		0x45, 0,0,0,
+	}
+	expected := state{
+		0x8E, 0, 0, 0,
+		0x4D, 0, 0, 0,
+		0xA1, 0,0,0,
+		0xBC, 0,0,0,
+	}
+
+	c.Check(mixOneColumn(before, 0), DeepEquals, expected)
+}
+
+func (s *MixColumnsSuite) Test_mixOneColumnTestVector2(c *C) {
+	before := state{
+		0xF2, 0, 0, 0,
+		0x0A, 0, 0, 0,
+		0x22, 0,0,0,
+		0x5C, 0,0,0,
+	}
+	expected := state{
+		0x9F, 0, 0, 0,
+		0xDC, 0, 0, 0,
+		0x58, 0,0,0,
+		0x9D, 0,0,0,
+	}
+
+	c.Check(mixOneColumn(before, 0), DeepEquals, expected)
+}
+
+func (s *MixColumnsSuite) Test_mixOneColumnTestVector3(c *C) {
+	before := state{
+		0x01, 0, 0, 0,
+		0x01, 0, 0, 0,
+		0x01, 0,0,0,
+		0x01, 0,0,0,
+	}
+	expected := state{
+		0x01, 0, 0, 0,
+		0x01, 0, 0, 0,
+		0x01, 0,0,0,
+		0x01, 0,0,0,
+	}
+
+	c.Check(mixOneColumn(before, 0), DeepEquals, expected)
+}
+
+func (s *MixColumnsSuite) Test_mixOneColumnTestVector4(c *C) {
+	before := state{
+		0, 0xC6, 0, 0,
+		0, 0xC6, 0, 0,
+		0, 0xC6, 0,0,
+		0, 0xC6, 0,0,
+	}
+	expected := state{
+		0, 0xC6, 0, 0,
+		0, 0xC6, 0, 0,
+		0, 0xC6, 0,0,
+		0, 0xC6, 0,0,
+	}
+
+	c.Check(mixOneColumn(before, 1), DeepEquals, expected)
+}
+
+func (s *MixColumnsSuite) Test_mixOneColumnTestVector5(c *C) {
+	before := state{
+		0, 0, 0xD4, 0,
+		0, 0, 0xD4, 0,
+		0, 0, 0xD4, 0,
+		0, 0, 0xD5, 0,
+	}
+	expected := state{
+		0, 0, 0xD5, 0,
+		0, 0, 0xD5, 0,
+		0, 0, 0xD7, 0,
+		0, 0, 0xD6, 0,
+	}
+
+	c.Check(mixOneColumn(before, 2), DeepEquals, expected)
+}
+
+func (s *MixColumnsSuite) Test_mixOneColumnTestVector6(c *C) {
+	before := state{
+		0, 0, 0x2D, 0,
+		0, 0, 0x26, 0,
+		0, 0, 0x31, 0,
+		0, 0, 0x4C, 0,
+	}
+	expected := state{
+		0, 0, 0x4D, 0,
+		0, 0, 0x7E, 0,
+		0, 0, 0xBD, 0,
+		0, 0, 0xF8, 0,
+	}
+
+	c.Check(mixOneColumn(before, 2), DeepEquals, expected)
+}
+
+func (s *MixColumnsSuite) Test_mixColumnTestVector(c *C) {
+	before := state{
+		0xDB, 0xC6, 0x2D, 0x01,
+		0x13, 0xC6, 0x26, 0x01,
+		0x53, 0xC6, 0x31, 0x01,
+		0x45, 0xC6, 0x4C, 0x01,
+	}
+	expected := state{
+		0x8E, 0xC6, 0x4D, 0x01,
+		0x4D, 0xC6, 0x7E, 0x01,
+		0xA1, 0xC6, 0xBD, 0x01,
+		0xBC, 0xC6, 0xF8, 0x01,
+	}
+
+	c.Check(mixColumns(before), DeepEquals, expected)
+}
