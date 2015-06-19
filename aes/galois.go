2227b5bbdd3560c3c41aff06c2ea5f26abdc0e02
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-06-19 10:24:47 -0500

    move all Galois related functions to the same place

--- aes/galois.go
+++ aes/galois.go
@@ -21,3 +21,37 @@ func galoisExp(x, n byte) byte {
 
 	return result
 }
+
+func multiplication(i, j byte) uint16 {
+	var out uint16 = 0
+	a := uint16(i)
+	b := uint16(j)
+
+	for place := uint16(0); place < 8; place++ {
+    var k uint16 = 1 << place
+    // if bit K in b is set, mask will be 0xFF, otherwise 0x00 - this avoids branching
+    mask := uint16((int16(k&b) << (15 - place)) >> 15)
+    out = out ^ ((a << place) & mask)
+	}
+
+	return out
+}
+
+func modulo(multiplied, poly uint16) byte {
+	out := uint16(multiplied)
+	b := uint16(poly)
+	for nbits(out) >= nbits(b) {
+		out = out ^ (b << (nbits(out) - nbits(b)))
+	}
+	return byte(out)
+}
+
+func nbits(a uint16) uint {
+	n := uint(0)
+
+	for ; a > 0; a >>= 1 {
+		n++
+	}
+
+	return n
+}
