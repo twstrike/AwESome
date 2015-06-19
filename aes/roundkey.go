76b0bc0841cd532f6890143d20eeafc51a98bda2
Fan Jiang <fanjiang@thoughtworks.com>
2015-06-19 16:39:50 -0500

    add AddRoundKey implement

--- aes/roundkey.go
+++ aes/roundkey.go
@@ -1,6 +1,13 @@
 package aes
 
 func addRoundKey(s state, rs roundSchedule) state {
-	// TODO: implement
-	return s
+	out := state{}
+	for i, si := range s {
+		column := i % 4
+		row := i / 4
+		shift := uint(24 - 8*(row))
+		word := byte(rs[column] >> shift)
+		out[i] = si ^ word
+	}
+	return out
 }
