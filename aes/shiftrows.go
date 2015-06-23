dd5379cc46a5d1323b9c0eb51355e4d5134a7947
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-06-23 08:41:36 -0500

    Add invShiftRow implementation

--- aes/shiftrows.go
+++ aes/shiftrows.go
@@ -14,3 +14,20 @@ func shiftRows(s state) state {
 	leftShift(&result, 3*4, 3)
 	return result
 }
+
+func rightShift(s *state, at, n int) {
+	left := make([]byte, n)
+	rowStart := at + n - 4
+
+	copy(left, s[at:])
+	copy(s[rowStart+n:at+n], s[rowStart:at])
+	copy(s[rowStart:], left)
+}
+
+func invShiftRows(s state) state {
+	result := s
+	rightShift(&result, 2*4-1, 1)
+	rightShift(&result, 3*4-2, 2)
+	rightShift(&result, 4*4-3, 3)
+	return result
+}
--- aes/shiftrows_test.go
+++ aes/shiftrows_test.go
@@ -25,3 +25,21 @@ func (s *ShiftRowsSuite) TestShiftRowsWithSimpleData(c *C) {
 
 	c.Check(shiftRows(input), DeepEquals, expected)
 }
+
+func (s *ShiftRowsSuite) TestInvShiftRowsWithSimpleData(c *C) {
+	input := state{
+		0, 1, 2, 3,
+		5, 6, 7, 4,
+		10, 11, 8, 9,
+		15, 12, 13, 14,
+	}
+
+	expected := state{
+		0, 1, 2, 3,
+		4, 5, 6, 7,
+		8, 9, 10, 11,
+		12, 13, 14, 15,
+	}
+
+	c.Check(invShiftRows(input), DeepEquals, expected)
+}
