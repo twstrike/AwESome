5de805377c9729a1f2429edf681cc4661a0693f3
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-06-26 13:38:05 -0500

    add example for when the padding adds a new block to the input

--- padding/standard_test.go
+++ padding/standard_test.go
@@ -11,14 +11,20 @@ type PaddingSuite struct{}
 
 var _ = Suite(&PaddingSuite{})
 
-func (s *PaddingSuite) TestPKCS7Padding(c *C) {
+func (s *PaddingSuite) TestPKCS7PaddingSingleBlockAndLesserThanBlockSize(c *C) {
 	data := []byte{0xaa, 0xbb, 0xcc}
 	result := PKCS7(data, 8)
 	c.Assert([]byte{0xaa, 0xbb, 0xcc, 0x5, 0x5, 0x5, 0x5, 0x5}, DeepEquals, result)
 }
 
-func (s *PaddingSuite) TestPKCS7Padding2(c *C) {
+func (s *PaddingSuite) TestPKCS7PaddingMultipleBlocksAndLesserThanBlockSize(c *C) {
 	data := []byte{0xaa, 0xbb, 0xcc}
 	result := PKCS7(data, 2)
 	c.Assert([]byte{0xaa, 0xbb, 0xcc, 0x01}, DeepEquals, result)
+}
+
+func (s *PaddingSuite) TestPKCS7PaddingCreatesAdditionalBlockWhenInputIsMultipleOfBlockSize(c *C) {
+	data := []byte{0xaa, 0xbb, 0xcc, 0xdd}
+	result := PKCS7(data, 2)
+	c.Assert([]byte{0xaa, 0xbb, 0xcc, 0xdd, 0x02, 0x02}, DeepEquals, result)
 }
