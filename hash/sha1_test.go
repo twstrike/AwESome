aa33402b98c9ad292686a94e5dd0b22a43fce246
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-06-30 08:59:08 -0500

    check all return values in readExactly tests

--- hash/sha1_test.go
+++ hash/sha1_test.go
@@ -11,15 +11,20 @@ func (s *SHA1Suite) TestReadExactly(c *C) {
 	input := []byte{0x11, 0x12, 0x11, 0x12, 0x11, 0x12, 0x11, 0x12, 0x11, 0x12, 0x11, 0x12, 0x11, 0x12}
 	reader := bytes.NewBuffer(input)
 	into := [5]byte{}
-	readExactly(into[:], reader)
+
+	n, err := readExactly(into[:], reader)
 	c.Assert(into[:], DeepEquals, input[:5])
+	c.Assert(n, Equals, len(into))
+	c.Assert(err, DeepEquals, nil)
 }
 
 func (s *SHA1Suite) TestReadExactlyWithEOFError(c *C) {
 	input := []byte{0x11, 0x12, 0x11, 0x12}
 	reader := bytes.NewBuffer(input)
 	into := [5]byte{}
-	_, err := readExactly(into[:], reader)
+
+	n, err := readExactly(into[:], reader)
 	c.Assert(into[:], DeepEquals, []byte{0x11, 0x12, 0x11, 0x12, 0x00})
+	c.Assert(n, Equals, len(input))
 	c.Assert(err, DeepEquals, io.EOF)
 }
