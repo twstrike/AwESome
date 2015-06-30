25d8b891ebd8f7177f343f9b28a648cdaacb3919
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-06-30 18:45:57 -0500

    Add SHA1MessageReader to expose an io.Reader that handles SHA1 padding

---
+++ hash/sha1msgreader.go
@@ -0,0 +1,50 @@
+package hash
+
+import (
+	"encoding/binary"
+	"io"
+
+	. "github.com/twstrike/AwESome/io"
+)
+
+// SHA1MessageReader implements SHA-1 padding for an io.Reader object.
+type SHA1MessageReader struct {
+	r    io.Reader
+	size uint64
+}
+
+// NewSha1MessageReader returns new SHA1MessageReader wrapping r
+func NewSha1MessageReader(r io.Reader) *SHA1MessageReader {
+	return &SHA1MessageReader{r: NewTrustworthyReader(r)}
+}
+
+func (r *SHA1MessageReader) addMessageLength(buffer []byte) {
+	w := NewSliceWriter(buffer[len(buffer)-8:])
+	binary.Write(w, binary.BigEndian, r.size)
+}
+
+// Read reads exactly sha1BlockSizeInBytes bytes into p
+func (r *SHA1MessageReader) Read(p []byte) (int, error) {
+	l, _ := r.r.Read(p[:sha1BlockSizeInBytes])
+	r.size += uint64(l * 8)
+
+	switch {
+	case l == 0:
+		if r.size%sha1BlockSizeInBytes == 0 {
+			p[l] = 0x80
+		}
+		r.addMessageLength(p)
+		return sha1BlockSizeInBytes, io.EOF
+	case l == sha1BlockSizeInBytes:
+		return sha1BlockSizeInBytes, nil
+	case l < (sha1BlockSizeInBytes - 9):
+		p[l] = 0x80
+		r.addMessageLength(p)
+		return sha1BlockSizeInBytes, io.EOF
+	case l < sha1BlockSizeInBytes:
+		p[l] = 0x80
+		return sha1BlockSizeInBytes, nil
+	}
+
+	return sha1BlockSizeInBytes, io.EOF
+}
