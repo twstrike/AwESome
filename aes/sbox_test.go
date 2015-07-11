bf8ff97994d5ec8bbf1704c25148dad75013b6e5
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-07-11 14:06:18 -0500

    Replace SBox calculations by an SBox table
    Benchmarks:
    
    (before)
    twstrike/AwESome/aes % go test -check.b
    PASS: encrypt_test.go:20: EncryptSuite.BenchmarkEncrypt128     500   5575950 ns/op
    PASS: encrypt_test.go:37: EncryptSuite.BenchmarkEncrypt192     500   6079463 ns/op
    OK: 2 passed
    
    (after)
    twstrike/AwESome/aes % go test -check.b
    PASS: encrypt_test.go:20: EncryptSuite.BenchmarkEncrypt128  200000      9877 ns/op
    PASS: encrypt_test.go:37: EncryptSuite.BenchmarkEncrypt192  200000     10628 ns/op
    OK: 2 passed

--- aes/sbox_test.go
+++ aes/sbox_test.go
@@ -8,6 +8,13 @@ type SBoxTestSuite struct{}
 
 var _ = Suite(&SBoxTestSuite{})
 
+func (*SBoxTestSuite) TestSboxTable(c *C) {
+	for i := 0; i <= 256; i++ {
+		b := byte(i)
+		c.Assert(sboxTable[b], Equals, calculateSBox(b))
+	}
+}
+
 func (s *SBoxTestSuite) TestAffineTransformation(c *C) {
 	c.Assert(affineTrans(byte(0x00)), Equals, byte(0x63))
 	c.Assert(affineTrans(byte(0xca)), Equals, byte(0xed))
@@ -16,25 +23,6 @@ func (s *SBoxTestSuite) TestAffineTransformation(c *C) {
 func (s *SBoxTestSuite) TestInvAffineTransformation(c *C) {
 	c.Assert(invAffineTrans(byte(0x63)), Equals, byte(0x00))
 	c.Assert(invAffineTrans(byte(0xed)), Equals, byte(0xca))
-}
-
-func (s *SubBytesTestSuite) TestApplySBox(c *C) {
-	c.Assert(applySBox(0x00), Equals, byte(0x63))
-	c.Assert(applySBox(0x11), Equals, byte(0x82))
-	c.Assert(applySBox(0x22), Equals, byte(0x93))
-	c.Assert(applySBox(0x33), Equals, byte(0xc3))
-	c.Assert(applySBox(0x44), Equals, byte(0x1b))
-	c.Assert(applySBox(0x55), Equals, byte(0xfc))
-	c.Assert(applySBox(0x66), Equals, byte(0x33))
-	c.Assert(applySBox(0x77), Equals, byte(0xf5))
-	c.Assert(applySBox(0x88), Equals, byte(0xc4))
-	c.Assert(applySBox(0x99), Equals, byte(0xee))
-	c.Assert(applySBox(0xaa), Equals, byte(0xac))
-	c.Assert(applySBox(0xbb), Equals, byte(0xea))
-	c.Assert(applySBox(0xcc), Equals, byte(0x4b))
-	c.Assert(applySBox(0xdd), Equals, byte(0xc1))
-	c.Assert(applySBox(0xee), Equals, byte(0x28))
-	c.Assert(applySBox(0xff), Equals, byte(0x16))
 }
 
 func (s *SubBytesTestSuite) TestApplyInvSBox(c *C) {
