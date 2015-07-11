00dd2204eb9f89f385ff8226ab24cf39055f006b
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-07-11 14:28:05 -0500

    Replace inverse SBox calculations by an inverse SBox table
    Benchmarks:
    
    (before)
    twstrike/AwESome/aes % go test -check.b
    PASS: decrypt_test.go:20: DecryptSuite.BenchmarkDecrypt128     500   4742672 ns/op
    PASS: encrypt_test.go:20: EncryptSuite.BenchmarkEncrypt128  200000      9723 ns/op
    PASS: encrypt_test.go:37: EncryptSuite.BenchmarkEncrypt192  200000     10519 ns/op
    OK: 3 passed
    
    (after)
    twstrike/AwESome/aes % go test -check.b
    PASS: decrypt_test.go:20: DecryptSuite.BenchmarkDecrypt128   50000     65238 ns/op
    PASS: encrypt_test.go:20: EncryptSuite.BenchmarkEncrypt128  200000     10040 ns/op
    PASS: encrypt_test.go:37: EncryptSuite.BenchmarkEncrypt192  200000     10521 ns/op
    OK: 3 passed

--- aes/sbox_test.go
+++ aes/sbox_test.go
@@ -12,6 +12,10 @@ func (*SBoxTestSuite) TestSboxTable(c *C) {
 	for i := 0; i <= 256; i++ {
 		b := byte(i)
 		c.Assert(sboxTable[b], Equals, calculateSBox(b))
+		c.Assert(invSboxTable[b], Equals, calculateInvSBox(b))
+
+		c.Assert(invSboxTable[sboxTable[b]], Equals, b)
+		c.Assert(sboxTable[invSboxTable[b]], Equals, b)
 	}
 }
 
@@ -23,23 +27,4 @@ func (s *SBoxTestSuite) TestAffineTransformation(c *C) {
 func (s *SBoxTestSuite) TestInvAffineTransformation(c *C) {
 	c.Assert(invAffineTrans(byte(0x63)), Equals, byte(0x00))
 	c.Assert(invAffineTrans(byte(0xed)), Equals, byte(0xca))
-}
-
-func (s *SubBytesTestSuite) TestApplyInvSBox(c *C) {
-	c.Assert(applyInvSBox(0x63), Equals, byte(0x00))
-	c.Assert(applyInvSBox(0x82), Equals, byte(0x11))
-	c.Assert(applyInvSBox(0x93), Equals, byte(0x22))
-	c.Assert(applyInvSBox(0xc3), Equals, byte(0x33))
-	c.Assert(applyInvSBox(0x1b), Equals, byte(0x44))
-	c.Assert(applyInvSBox(0xfc), Equals, byte(0x55))
-	c.Assert(applyInvSBox(0x33), Equals, byte(0x66))
-	c.Assert(applyInvSBox(0xf5), Equals, byte(0x77))
-	c.Assert(applyInvSBox(0xc4), Equals, byte(0x88))
-	c.Assert(applyInvSBox(0xee), Equals, byte(0x99))
-	c.Assert(applyInvSBox(0xac), Equals, byte(0xaa))
-	c.Assert(applyInvSBox(0xea), Equals, byte(0xbb))
-	c.Assert(applyInvSBox(0x4b), Equals, byte(0xcc))
-	c.Assert(applyInvSBox(0xc1), Equals, byte(0xdd))
-	c.Assert(applyInvSBox(0x28), Equals, byte(0xee))
-	c.Assert(applyInvSBox(0x16), Equals, byte(0xff))
 }
